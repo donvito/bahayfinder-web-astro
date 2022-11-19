3a6ca566af4f1613bcd46dcef8d289763fb75e33
Melvin Vivas <melvindave@gmail.com>
2022-11-19 18:07:54 +0800

    Initial commit

---
+++ theme/src/config/footer.js
@@ -0,0 +1,52 @@
+export const footerSocials = [
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/donvito',
+    icon: "mdi:twitter",
+  },
+  {
+    name: 'Youtube',
+    url: 'https://youtube.com/c/DonvitoCodes',
+    icon: "mdi:youtube",
+  },
+  {
+    name: 'Facebook',
+    url: 'https://www.facebook.com/donvitocodes',
+    icon: "ic:baseline-facebook",
+  },
+  {
+    name: 'Reddit',
+    url: 'https://www.reddit.com/r/donvitocodes/',
+    icon: "ic:baseline-reddit",
+  },  
+  {
+    name: 'Github',
+    url: 'https://github.com/donvito',
+    icon: "mdi:github",
+  },
+  {
+    name: 'Discord',
+    url: 'https://discord.gg/udjsk9aMjE',
+    icon: "ic:baseline-discord",
+  },
+]
+
+export const footerLists = [	
+	{
+		title: 'Quick Links',
+		items: [
+			{
+				title: 'About',
+				slug: '/company/about',
+			},
+			{
+				title: 'Blog',
+				slug: '/blog',
+			},
+			{
+				title: 'Contact',
+				slug: '/company/contact',
+			},
+		],
+	}
+];
